0d92b962ab7a7bb11c8b1275f13b1f6b335deded
CD Yang <nwpu409@gmail.com>
2019-12-16 22:53:08 +0800

    add test controller

---
+++ HelloWeb/Controllers/MyTestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace HelloWeb.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MyTestController : ControllerBase
+    {
+        private readonly ILogger<MyTestController> _logger;
+
+        public MyTestController(ILogger<MyTestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello. This is my controller.";
+        }
+    }
+}
